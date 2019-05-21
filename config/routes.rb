37430a2aca1997c6da35ceee5c4f3d0accbdcbaa
Jake Spector <spector.jake@gmail.com>
2019-05-21 15:03:30 +0800

    Adding edit and delete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
